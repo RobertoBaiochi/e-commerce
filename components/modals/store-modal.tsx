863bd068f7324150f013dabc56ecfc62f4ecb199
RobertoBaiochi <robertombbaiochi@gmail.com>
2023-09-19 11:29:15 -0300

    first commit

---
+++ components/modals/store-modal.tsx
@@ -0,0 +1,94 @@
+"use client";
+
+import { useState } from "react";
+import axios from "axios";
+import * as z from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { Modal } from "@/components/ui/modal";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import toast from 'react-hot-toast';
+
+const formSchema = z.object({
+  name: z.string().min(1, "O nome da loja deve conter ao menos 1 caractere"),
+});
+
+export const StoreModal = () => {
+  const storeModal = useStoreModal();
+  const [loading, setLoading] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("/api/stores", values);
+      toast.success("Loja criada com sucesso")
+    } catch (error) {
+      console.log("[STORE_ONSUBMIT]", error);
+      toast.error("Erro ao criar loja");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Modal
+      title="Criar Loja"
+      description="Adicione uma nova loja para gerenciar produtos e categorias"
+      isOpen={storeModal.isOpen}
+      onClose={storeModal.onClose}
+    >
+      <div>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Nome</FormLabel>
+                    <FormControl>
+                      <Input
+                        disabled={loading}
+                        placeholder="E-Commerce"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage></FormMessage>
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end">
+                <Button variant="outline" onClick={storeModal.onClose}>
+                  Cancelar
+                </Button>
+                <Button disabled={loading} type="submit">
+                  Continuar
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </div>
+    </Modal>
+  );
+};
